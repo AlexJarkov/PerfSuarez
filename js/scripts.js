b77044cc775b2409e6824fb4918c990f0064ad75
alexjarkov666 <asfernandezkharkovskaya@gmail.com>
2024-10-27 22:11:40 -0400

    PerfSuarez 2.0beta-5

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const combos = document.querySelectorAll('.combo');
+
+    combos.forEach(combo => {
+        combo.addEventListener('click', (e) => {
+            e.preventDefault();
+            const whatsappNumber = combo.getAttribute('data-whatsapp');
+            const nomCombo = combo.getAttribute('data-combo');
+            const confirmation = confirm('¿Desea continuar a WhatsApp para realizar su consulta?');
+
+            if (confirmation) {
+                window.open(`https://wa.me/${whatsappNumber}/?text=Hola!%20Quisiera%20saber%20si%20tienen%20disponible%20el%20${nomCombo}!`, '_blank');
+            }
+        });
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const perfumes = document.querySelectorAll('.perfume');
+
+    // Añade un filtro basado en etiquetas
+    const filterPerfumes = (tag) => {
+        perfumes.forEach(perfume => {
+            if (perfume.getAttribute('data-tags').includes(tag)) {
+                perfume.style.display = 'block';
+            } else {
+                perfume.style.display = 'none';
+            }
+        });
+    };
+
+    // Ejemplo de uso del filtro
+    // filterPerfumes('mas-vendido'); // Muestra solo los perfumes con la etiqueta "más vendido"
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
+});
