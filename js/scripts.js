1eac65365057bff1f11383d3025042093ffc4cc1
AlexJarkov <asfernandezkharkovskaya@gmail.com>
2025-09-15 14:44:00 -0400

    Fixed again

--- js/scripts.js
+++ js/scripts.js
@@ -39,7 +39,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menu-toggle');
     const menu = document.getElementById('menu');
 
-    menuToggle.addEventListener('click', () => {
-        menu.classList.toggle('show');
-    });
+    if (menuToggle && menu) {
+        menuToggle.addEventListener('click', () => {
+            menu.classList.toggle('show');
+        });
+    }
 });
